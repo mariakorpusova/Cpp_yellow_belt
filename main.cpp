cf9b909b144b20f66d091826432d49a6957f4060
Maria <Maria@mail>
2019-06-30 14:02:35 +0300

    Rectangle header

--- main.cpp
+++ main.cpp
@@ -1,10 +1,5 @@
 #include <iostream>
-#include "phone_number.h"
 
 int main(){
-    PhoneNumber pn("+7-495-111-22-33");
-    cout << pn.GetCountryCode() << endl;
-    cout << pn.GetCityCode() << endl;
-    cout << pn.GetLocalNumber() << endl;
-    cout << pn.GetInternationalNumber() << endl;
+
 }
