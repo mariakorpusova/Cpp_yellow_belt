3b4cfc3446870a7739fa3ffc4b202da65a2e5e3d
Maria <Maria@mail>
2019-06-30 15:44:38 +0300

    Part of vector out

--- main.cpp
+++ main.cpp
@@ -1,5 +1,31 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void PrintVectorPart(const vector<int>& numbers);
 
 int main(){
+
+	  PrintVectorPart({6, 1, 8, -5, 4});
+	  cout << endl;
+	  PrintVectorPart({-6, 1, 8, -5, 4});  // ничего не выведется
+	  cout << endl;
+	  PrintVectorPart({6, 1, 8, 5, 4});
+	  cout << endl;
+	  return 0;
+
+}
+
+void PrintVectorPart(const vector<int>& numbers){
+
+	auto res = find_if(begin(numbers), end(numbers),
+			[](const int& num){ return num < 0;});
+
+	while( res > begin(numbers)){
+		res--;
+		cout << *res << " ";
+	}
 
 }
